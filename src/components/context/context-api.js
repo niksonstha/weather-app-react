c220992613868c609ca981f5024d334a6bd89db7
niksonstha <sthanikson10@gmail.com>
2023-06-26 23:30:52 +0545

    deploy

--- src/components/context/context-api.js
+++ src/components/context/context-api.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { debounce } from "lodash";
 
 const ApiContext = React.createContext();
 
 const ApiProvider = ({ children }) => {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState("kathmandu");
+  let timeout;
 
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=633507c31bea1f22fb71174fc6fd6c62&units=metric`;
-
-  // Debounce the location updates
-  const debouncedSetLocation = debounce(setLocation, 500);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,8 +24,11 @@ const ApiProvider = ({ children }) => {
   }, [URL]);
 
   const handleLocationChange = (newLocation) => {
-    // Update the location using the debounced function
-    debouncedSetLocation(newLocation);
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      setLocation(newLocation);
+    }, 500);
   };
 
   return (
